f62d583f466d74ba56b1e9319f00c1e9fc108cd3
jgrissom <jgrissom91@gmail.com>
2021-07-05 22:11:41 -0500

    reset pymon  - game starts when green button is pressed

--- main.py
+++ main.py
@@ -3,20 +3,33 @@ from async_switch import Switch
 from machine import Pin
 from output import Output
 
+# define globals
+READY = 0
+ACTIVE = 1
+BUSY = 2
+GREEN = 1
+
 class Pymon():
     def __init__(self):
         self.btns = [ Switch( Pin(18, Pin.IN, Pin.PULL_UP) ), Switch( Pin(5, Pin.IN, Pin.PULL_UP) ), Switch( Pin(22, Pin.IN, Pin.PULL_UP) ), Switch( Pin(21, Pin.IN, Pin.PULL_UP) ) ]
         self.leds = [ Output( Pin(26, Pin.OUT) ), Output( Pin(27, Pin.OUT) ), Output( Pin(15, Pin.OUT) ), Output( Pin(14, Pin.OUT) ) ]
         for led in self.leds:
             led.off()
+            
+        self.status = BUSY
+    def reset(self):
+        self.leds[GREEN].on()
+        self.status = READY
         
 def press_button(idx):
-    pymon.leds[idx].flicker(.2)
+    if pymon.status == READY and idx == GREEN:
+        print('start game')
 
 async def main():
     for i in range(len(pymon.btns)):
         pymon.btns[i].open_func(press_button, (i,))
     
+    pymon.reset()
     while True:
         await asyncio.sleep(.01)
 
